3266379398d6df2976650973ffac92ea638fc34d
João Lucas <joo.lucas994@gmail.com>
2023-02-03 19:18:00 -0300

    Projeto criado

---
+++ src/test/java/com/joaolucas/coursespringboot/CourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

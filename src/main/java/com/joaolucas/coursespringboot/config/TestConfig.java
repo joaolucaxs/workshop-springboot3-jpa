b14fb9e6b336e53ebf6f7e95c06a671fe042262d
João Lucas <joo.lucas994@gmail.com>
2023-02-04 19:41:46 -0300

    Category - Repository, Service, Resource

--- src/main/java/com/joaolucas/coursespringboot/config/TestConfig.java
+++ src/main/java/com/joaolucas/coursespringboot/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.joaolucas.coursespringboot.entities.Category;
 import com.joaolucas.coursespringboot.entities.Order;
 import com.joaolucas.coursespringboot.entities.User;
 import com.joaolucas.coursespringboot.entities.enums.OrderStatus;
+import com.joaolucas.coursespringboot.repositories.CategoryRepository;
 import com.joaolucas.coursespringboot.repositories.OrderRepository;
 import com.joaolucas.coursespringboot.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	
 	@Override
@@ -39,6 +44,12 @@ public class TestConfig implements CommandLineRunner{
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 }
---
+++ src/main/java/com/joaolucas/coursespringboot/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.joaolucas.coursespringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaolucas.coursespringboot.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+	
+}

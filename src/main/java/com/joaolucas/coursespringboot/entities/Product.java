9fb1e50bd3305bef00e9ddfff4759894cce2c390
João Lucas <joo.lucas994@gmail.com>
2023-02-05 16:50:56 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/joaolucas/coursespringboot/entities/Product.java
+++ src/main/java/com/joaolucas/coursespringboot/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,14 +14,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -29,10 +32,11 @@ public class Product implements Serializable{
 	private String imgUrl;
 
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 
@@ -90,6 +94,16 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
+	
+	@JsonIgnore
+	public Set<Order> getOrder() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -113,7 +127,5 @@ public class Product implements Serializable{
 		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
 				+ ", imgUrl=" + imgUrl + ", categories=" + categories + "]";
 	}
-	
-	
 
 }

d68e65ac60b65c2ed7de2c44f64f1f47c36dd898
João Lucas <joo.lucas994@gmail.com>
2023-02-06 16:13:53 -0300

    Exception handling - delete

--- src/main/java/com/joaolucas/coursespringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/joaolucas/coursespringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.joaolucas.coursespringboot.services.exceptions.DataBaseException;
 import com.joaolucas.coursespringboot.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,5 +22,15 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> databaseError(DataBaseException e, HttpServletRequest request){
+		String error = "DataBase Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
 	
 }
---
+++ src/main/java/com/joaolucas/coursespringboot/services/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.joaolucas.coursespringboot.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+	
+}
